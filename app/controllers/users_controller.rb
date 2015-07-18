17577f640381617d2ad137163fdbaaf7486d7511
Sonph <evilsp4@gmail.com>
2015-07-18 19:48:29 +0700

    sign up user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,25 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
+
+  def create
+    @user = User.new(params_user)
+    if @user.save
+      redirect_to @user
+    else
+      # binding.pry
+      render 'new'
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+    def params_user
+      params.require(:user).permit(:email, :name, :password, :password_confirm)
+      
+    end
 end
