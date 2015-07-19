8c079c28cbd3973cf145a2ee7a0b1749c3049be5
Sonph <evilsp4@gmail.com>
2015-07-19 10:07:08 +0700

    add remember me

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -7,6 +7,7 @@ class SessionController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       log_in user
       redirect_to user
     else
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,29 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+
+  attr_accessor :remember_token
+
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def self.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    self.update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def authenticate?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    self.update_attribute(:remember_digest, nil)
+  end
 end
