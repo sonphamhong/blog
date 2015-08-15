ffa711be56a3d831a3ea07256e5d05c5fdbaca98
Sonph <evilsp4@gmail.com>
2015-08-15 11:07:58 +0700

    minor fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     @posts = Post.order(:likes_count)
-    @hash_data = []
-    @posts.in_groups_of(2) {|group| @hash_data.push(group)}
+    # @hash_data = []
+    # @posts.in_groups_of(2) {|group| @hash_data.push(group)}
   end
 end
